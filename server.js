792eee2802d04a4d7a7e2073a51538b6924f0dd6
zhangyu <zhangyu@teambition.com>
2019-02-28 15:10:53 +0800

    fix

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
-
+const fs =require('fs')
+const path = require('path')
 const app = express()
 
+app.use(express.static((path.join(__dirname), './dist')))
+
+app.use('*', (req, res) => {
+  fs.readFileSync(path.join(__dirname, './dist/index.html'), (err, data) => {
+    if(err) throw err
+    res.send(data)
+  })
+})
+
 app.listen(3001, () => {
-    console.log('server is running at localhost:3000');
+    console.log('server is running at localhost:3001');
 })
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { observable, computed, action, inject } from 'mobx';
+import { observable, computed, action } from 'mobx';
 import { observer } from 'mobx-react';
 import { Foo } from './Foo'
 
