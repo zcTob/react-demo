45db0125f842187877ae5ff52728852f2e46c686
zhangyu <zhangyu@teambition.com>
2019-06-19 18:34:33 +0800

    f

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ let config
 console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
   config = {
-    baseUrl: '//api.hellozhangyu.top:3001/api'
+    baseUrl: '//api.hellozhangyu.top/api'
   }
 } else {
   config = {
