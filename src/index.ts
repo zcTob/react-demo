f4147171cbd667ad2ce2cd341a922efcfae4de19
zhangyu <zhangyu@teambition.com>
2019-07-25 14:38:38 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom'
 import 'normalize.css'
 import 'github-markdown-css/github-markdown.css'
 import App from './views/App'
-import './app.js'
 // import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
