a2a16f0913e4d779361e31bc986556e0ce1ff891
zhangyu <419657816@qq.com>
2019-07-25 15:14:07 +0800

    ssr

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,9 +1,10 @@
 const wbepack = require('webpack')
-const merge = require('webpack-merge');
+const merge = require('webpack-merge')
 const common = require('./common.js')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin
 const path = require('path')
 
 module.exports = (env) => {
@@ -26,19 +27,19 @@ module.exports = (env) => {
             path: path.resolve(__dirname, '../build'),
             publicPath: '/',
             chunkFilename: 'js/[name][chunkhash].chunk.js',
-            filename: 'js/[name][contenthash].js',
+            filename: 'js/[name][contenthash].js'
         },
         plugins: [
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, '../public/index.html'),
-                filename: path.resolve(__dirname, '../build/index.html'),
+                filename: path.resolve(__dirname, '../build/index.html')
             }),
             new MiniCssExtractPlugin({
                 // Options similar to the same options in webpackOptions.output
                 // both options are optional
-                filename: "css/[name].css",
-                chunkFilename: "css/[name].[chunkhash].css"
-            }),
+                filename: 'css/[name].css',
+                chunkFilename: 'css/[name].[chunkhash].css'
+            })
         ]
     })
 
